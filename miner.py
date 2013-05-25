16ecfb2488071abf21f46b096fbf3bf32828ecae
davidak <git@davidak.de>
2013-05-25 02:00:29 +0200

    add function

--- miner.py
+++ miner.py
@@ -4,9 +4,11 @@
 import random
 import string
 
-word = ''
 
-while word is not 'penis':
-	word = ''.join(random.choice(string.ascii_lowercase) for x in range(5))
+def mine():
+	word = ''
+	while word is not 'penis':
+		word = ''.join(random.choice(string.ascii_lowercase) for x in range(5))
+	print("generated 1 peniscoin")
 
-print("generated 1 peniscoin")
+mine()
