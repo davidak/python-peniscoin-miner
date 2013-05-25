a582155907460cf69eddb3354f1c75fd09874b13
davidak <git@davidak.de>
2013-05-25 03:17:36 +0200

    add performance monitoring

--- miner.py
+++ miner.py
@@ -3,15 +3,31 @@
 
 import random
 import string
-
+from datetime import datetime
+import os
 
 def mine():
+	start = datetime.now()
 	word = ''
 	i = 0
 	while word is not 'penis':
 		word = ''.join(random.choice(string.ascii_lowercase) for x in range(5))
-		print(str(i) + ' ' + word)
 		i += 1
-	print("generated 1 peniscoin")
+		if (i % 10000) == 0: # every 10.000 run
+			try: # ZeroDivision error workaround
+				now = datetime.now()
+				timediff = now - start
+				seconds = timediff.seconds # seconds the programm run
+				hashes =  '{0:.{1}f}'.format(i / seconds, 0) # Hash/s
+				os.system('cls' if os.name=='nt' else 'clear') # Unix and Windows clear screen
+				print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' ' + str(hashes) + ' Hash/s runs ' + str(seconds) + ' seconds')
+			except:
+				print('Error: computing performance data failed')
+		#print(str(i) + ' ' + word)
+	
+	end = datetime.now()
+	timediff = end - start
+	seconds = timediff.seconds # seconds the programm finally run
+	print('generated 1 PC in ' + str(seconds) + ' seconds. averange speed was ' + str(hashes) + ' Hash/s')
 
 mine()
