3c2563da0271bc665743d0a4cd6797a31002c05b
davidak <git@davidak.de>
2013-05-25 02:08:07 +0200

    add iteration counter and debug output

--- miner.py
+++ miner.py
@@ -7,8 +7,11 @@ import string
 
 def mine():
 	word = ''
+	i = 0
 	while word is not 'penis':
 		word = ''.join(random.choice(string.ascii_lowercase) for x in range(5))
+		print(str(i) + ' ' + word)
+		i += 1
 	print("generated 1 peniscoin")
 
 mine()
