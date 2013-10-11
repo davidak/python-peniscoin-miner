eee5d2e17f5832e0f817b97c7845d678d0b5df26
davidak <git@davidak.de>
2013-10-11 06:37:33 +0200

    Miner is now a class, added version

--- miner.py
+++ miner.py
@@ -1,43 +1,73 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
 
 import random
 import string
 import time
 import hashlib
+import pickle
 import os
+from version import __version__
 
-def mine():
-	start = time.clock()
-	key = 'f6952d6eef555ddd87aca66e56b91530222d6e318414816f3ba7cf5bf694bf0f'
-	word = ''
-	coins = 0
-	i = 0
-	while True:
-		while word != key:
-			word = hashlib.sha256(''.join(random.choice(string.ascii_lowercase) for x in range(5)).encode('utf-8')).hexdigest()
-			#print(str(i) + ' ' + word)
-			i += 1
-			if (i % 10000) == 0: # every 10.000 run
-				now = time.clock()
-				seconds = now - start # seconds the programm run
-				hashes =  int(i / seconds) # hashes / seconds
-				if hashes > 1000000:
-					hashes = '{0:.2f} GHash/s'.format(hashes / 1000000)
-				elif hashes > 1000:
-					hashes = '{0:.2f} MHash/s'.format(hashes / 1000)
+class Miner(object):
+	"""
+	Peniscoin-Miner
+	"""
+	def __init__(self):
+		# read balance and last hash from wallet.dat
+		for i in range(10):
+			try:
+				self.load_wallet()
+			except IOError: # if wallet.dat don't exists
+				self.create_wallet()
+
+	def __str__(self):
+		return self.hashes + ', ' + self.seconds + ' Sekunden'
+
+	def save_wallet(self, coins, last_hash):
+		with open('wallet.dat', 'wb') as f:
+			pickle.dump(coins, f)
+			pickle.dump(last_hash, f)
+
+	def load_wallet(self):
+		with open('wallet.dat', 'rb') as f:
+			self.coins = pickle.load(f)
+			self.last_hash = pickle.load(f)
+
+	def create_wallet(self):
+		self.save_wallet(0, 'f6952d6eef555ddd87aca66e56b91530222d6e318414816f3ba7cf5bf694bf0f')
+
+	def mine(self):
+		self.start = time.clock()
+		self.i = 0
+		while True:
+			self.current_hash = hashlib.sha256(''.join(random.choice(string.ascii_lowercase) for x in range(5)).encode('utf-8')).hexdigest()
+
+			self.i += 1
+
+			if (self.i % 10000) == 0: # every 10.000 run
+				self.now = time.clock()
+				self.seconds = self.now - self.start # seconds the programm run
+				self.hashes =  int(self.i / self.seconds) # hashes / seconds
+				if self.hashes > 1000000:
+					self.hashes = '{0:.2f} GHash/s'.format(self.hashes / 1000000)
+				elif self.hashes > 1000:
+					self.hashes = '{0:.2f} MHash/s'.format(self.hashes / 1000)
 				else:
-					hashes = '{0} Hash/s'.format(hashes)
+					self.hashes = '{0} Hash/s'.format(self.hashes)
 
 				os.system('cls' if os.name=='nt' else 'clear') # Unix and Windows clear screen
-				print(time.strftime('%Y-%m-%d %H:%M:%S ') + hashes + ' ' + str(coins) + ' Peniscoins mined in ' + '{0:.{1}f}'.format(seconds, 3) + ' seconds')
+				print(time.strftime('%Y-%m-%d %H:%M:%S ') + self.hashes + ' ' + str(self.coins) + ' Peniscoins mined in ' + '{0:.{1}f}'.format(self.seconds, 3) + ' seconds')
 
-			#if (i > 1000000): # test succesfull generation
-			#	word = hashlib.sha256('penis'.encode('utf-8')).hexdigest()
-		
-		coins += 1
-		print('Congratulations! You generated 1 Peniscoin.')
-		word = ''
-		time.sleep(3)
+			#if (self.i % 1000000) == 0: # test succesfull generation
+			#	self.current_hash = hashlib.sha256('penis'.encode('utf-8')).hexdigest()
 
-mine()
+			if self.current_hash == self.last_hash:
+				self.coins += 1
+				self.save_wallet(self.coins, self.current_hash)
+
+
+if __name__ == '__main__':
+	print("Peniscoin-Miner Version " + __version__)
+	m = Miner()
+	m.mine()
